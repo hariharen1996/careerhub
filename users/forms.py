d5f1b6c8f44968c35b135d63079feb53c83d3000
hariharen96 <hariharen1996@gmail.com>
2025-02-11 17:48:47 +0530

    feat:add login form with custom widgets
    feat:add login functionality login in views.py and add login route
    
    feat:add login.html template with csrf protection

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm 
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django import forms
 from .models import CustomUsers
 
@@ -7,3 +7,7 @@ class CustomUserForm(UserCreationForm):
     class Meta:
         model = CustomUsers
         fields = ['username','email','user_type','password1','password2']
+    
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Enter your username'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'Enter your password'}))
--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,HttpResponse,redirect
-from .forms import UserCreationForm
+from .forms import UserCreationForm,LoginForm
 
 # Create your views here.
 def register_view(request):
@@ -12,3 +12,13 @@ def register_view(request):
         forms = UserCreationForm()
 
     return render(request,'users/register.html',{'forms':forms})
+
+def login_view(request):
+    if request.method == 'POST':
+        forms = LoginForm(request,data=request.POST)
+        if forms.method == 'POST':
+            forms.save()
+            return redirect('login')
+    else:
+        forms = LoginForm()
+    return render(request,'users/login.html',{'forms':forms})
