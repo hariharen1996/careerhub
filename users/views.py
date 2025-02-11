79fc1c4fdafff5f9dd9ed7bc2ffe27c18c47b840
hariharen96 <hariharen1996@gmail.com>
2025-02-11 16:41:55 +0530

    feat: create urls.py in users app and add register route
    feat:include the users url in root projects urls.py using include function
    
    feat: add logic for register with HttpResponse in views.py

--- users/views.py
+++ users/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
+def register_view(request):
+    return HttpResponse("<h1>Register</h1>")
