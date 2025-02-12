fe63656b0d8ae12667115d1d9f576307fd1f3072
hariharen96 <hariharen1996@gmail.com>
2025-02-12 10:59:07 +0530

    chore:install crispy_bootstrap5, django_crispy_forms
    refactor: modify login,register with cripsy forms and bootstrap, refactor login functionality in views.py
    
    feat:add home page in job app
    
    feat:create urls.py in job app, include in root project urls

--- careerhub/settings.py
+++ careerhub/settings.py
@@ -41,7 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users',
     'job',
-    'django_bootstrap5'
+    'django_bootstrap5',
+    'crispy_bootstrap5',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -126,3 +128,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
--- users/views.py
+++ users/views.py
@@ -1,24 +1,33 @@
-from django.shortcuts import render,HttpResponse,redirect
-from .forms import UserCreationForm,LoginForm
+from django.shortcuts import render,redirect
+from .forms import CustomUserForm,LoginForm
+from django.contrib.auth import login,authenticate,logout
 
 # Create your views here.
 def register_view(request):
     if request.method == 'POST':
-        forms = UserCreationForm(request.POST)
+        forms = CustomUserForm(request.POST)
         if forms.is_valid():
             forms.save()
             return redirect('login')
     else:
-        forms = UserCreationForm()
+        forms = CustomUserForm()
 
     return render(request,'users/register.html',{'forms':forms,'title':'Register'})
 
 def login_view(request):
     if request.method == 'POST':
         forms = LoginForm(request,data=request.POST)
-        if forms.method == 'POST':
-            forms.save()
-            return redirect('login')
+        if forms.is_valid():
+            username = forms.cleaned_data.get('username')
+            password = forms.cleaned_data.get('password')
+            user = authenticate(request,username=username,password=password)
+            login(request,user)
+            if user is not None:
+                return redirect('job-home')    
     else:
         forms = LoginForm()
     return render(request,'users/login.html',{'forms':forms,'title':'Login'})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
